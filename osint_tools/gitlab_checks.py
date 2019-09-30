b5207f471cc44e8b51b6162b91032efb2104d01e
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2019-09-30 15:14:53 -0400

    remove unimplemented switches and related methods for now

--- osint_tools/gitlab_checks.py
+++ osint_tools/gitlab_checks.py
@@ -6,25 +6,9 @@ These are called from the parent program, with the two core functions being:
     - process_projects
 """
 
-import logging as l
+from logging import info, warning
 
 from api import gitlab
-
-
-def process_repos(repo):
-    """
-    Process a GitLab repo
-    """
-    l.info("Repos:  This feature is not yet implemented.")
-    return
-
-
-def process_projects(projects):
-    """
-    Process a GitLab project
-    """
-    l.info("Projects:  This feature is not yet implemented.")
-    return
 
 
 def process_groups(groups):
@@ -38,7 +22,7 @@ def process_groups(groups):
     for group in groups:
         group_details = gitlab.get_group(group)
         if not group_details:
-            l.warning("[!] %s not found, skipping", group)
+            warning("[!] %s not found, skipping", group)
             continue
 
         members = gitlab.get_group_members(group)
@@ -48,17 +32,16 @@ def process_groups(groups):
         group_projects = gitlab.get_group_projects(group)
 
         # Print / log all the gorey details
-        l.info("GROUP: %s (%s)", group_details['name'],
-               group_details['web_url'])
+        info("GROUP: %s (%s)", group_details['name'], group_details['web_url'])
 
-        l.info("  GROUP PROJECTS:")
+        info("  GROUP PROJECTS (%s):", len(group_projects))
         for link in group_projects:
-            l.info("    %s", link)
+            info("    %s", link)
 
-        l.info("  MEMBERS:")
+        info("  MEMBERS (%s):", len(members))
         for member in members:
-            l.info("    %s", member)
+            info("    %s", member)
 
-        l.info("  MEMBERS' PERSONAL PROJECTS:")
+        info("  MEMBERS' PERSONAL PROJECTS (%s):", len(personal_projects))
         for link in personal_projects:
-            l.info("    %s", link)
+            info("    %s", link)
--- utilities/arguments.py
+++ utilities/arguments.py
@@ -8,20 +8,12 @@ def parse():
     desc = "Collect OSINT from GitLab and GitHub"
     parser = argparse.ArgumentParser(description=desc)
 
-    # Any combination, including multiple of each, of projects,
-    # groups, repos, snippets, and teams.
     parser.add_argument('-g', '--group', type=str, action='append',
                         help='Name of a GitLab group')
     parser.add_argument('-s', '--snippets', type=str, action='append',
-                        help="Enable search for snippets in gitlab for secrets")
+                        help='Enable search for snippets in gitlab for secrets.  ')
     parser.add_argument('-l', '--logfile', type=str, action='store',
                         help='Will APPEND found items to specified file.')
-    # parser.add_argument('-p', '--project', type=str, action='append',
-    #                     help='Name of a GitLab project')
-    # parser.add_argument('-t', '--team', type=str, action='append',
-    #                     help='Name of a GitHub team')
-    # parser.add_argument('-r', '--repo', type=str, action='append',
-    #                     help='Name of a GitHub repo')
 
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
@@ -33,9 +25,3 @@ def parse():
 def apply_all(args):
     if args.group:
         gitlab_checks.process_groups(args.group)
-    # if args.project:
-    #     gitlab_checks.process_projects(args.group)
-    # if args.team:
-    #     gitlab_checks.process_team(args.team)
-    # if args.repo:
-    #     gitlab_checks.process_repos(args.repo)
