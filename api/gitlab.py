c9051db54bfe416f0014a83e0cb82f9bcca679cd
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2019-09-30 12:57:04 -0400

    isolate gitlab api requests

---
+++ api/gitlab.py
@@ -0,0 +1,139 @@
+import requests
+import os
+import re
+
+from logging import warning, info
+
+BASE_URL = 'https://gitlab.com/api/v4'
+
+
+def get_personal_projects(member):
+    """
+    Returns a list of all personal projects for a member
+    """
+    project_urls = []
+
+    info("[*] Fetching personal projects for %s", member)
+    details = get('{}/users/{}/projects'.format(BASE_URL, member))
+
+    if not details:
+        return []
+
+    for item in details:
+        project_urls.append(item['http_url_to_repo'])
+
+    return project_urls
+
+
+def get_group_projects(group):
+    """
+    Returns a list of all projects belonging to a group
+    """
+    project_urls = []
+
+    info("[*] Fetching group projects for %s", group)
+    details = get('{}/groups/{}/projects'.format(BASE_URL, group))
+
+    if not details:
+        pass
+
+    for item in details:
+        project_urls.append(item['http_url_to_repo'])
+
+    return project_urls
+
+
+def get_group(group):
+    """
+    Validates access to a group via the api
+    """
+    info("[*] Fetching group details for %s", group)
+    group_details = get('{}/groups/{}'.format(BASE_URL, group))
+
+    if not group_details:
+        return False
+
+    return group_details
+
+
+def get_group_members(group):
+    """
+    Returns a list of all members of a group
+    """
+    members = []
+
+    info("[*] Fetching group members for %s", group)
+    details = get('{}/groups/{}/members'.format(BASE_URL, group))
+
+    # We should now have a list of dictionary items, need to parse through
+    # each one to extract the member info.
+    for item in details:
+        members.append(item['username'])
+
+    return members
+
+
+def get_current_user():
+    details = get('{}/user'.format(BASE_URL))
+
+    if not details:
+        return False
+
+    username = details['username']
+    return username
+
+
+def get(url):
+    """
+    Helper function to interact with GitLab API using python requests
+
+    The important things here are:
+        - Adding the PRIVATE-TOKEN header based on env variable
+        - interacting with the pagination process via LINK headers
+          (https://docs.gitlab.com/ee/api/README.html#pagination)
+    """
+
+    headers = {'PRIVATE-TOKEN': os.getenv('GITLAB_API')}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        # The "Link" header is returned when there is more than one page of
+        # results. GitLab asks that we use this link instead of crafting
+        # our own.
+        if 'Link' in response.headers.keys():
+            # initialize a new variable to begin compounding multi-page
+            # results
+            all_results = response.json()
+            # Now, loop through until there is no 'next' link provided
+            pagenum = 2
+            while 'Link' in response.headers.keys() and 'rel="next"' in response.headers['Link']:
+                # Using print instead of logging, we don't want the per-page
+                # status update in the log file
+                print("[*] Processing page {}\r".format(pagenum))
+
+                regex = re.compile(r'<([^<>]*?)>; rel="next"')
+                next_url = re.findall(regex, response.headers['Link'])[0]
+
+                # Add the individual response to the collective
+                response = requests.get(next_url, headers=headers)
+                if response.status_code == 200:
+                    all_results += response.json()
+                else:
+                    warning("[!] Error processing pagination URL: %s", next_url)
+                pagenum += 1
+
+            # We need a line break if we've counted pages
+            if pagenum > 1:
+                print("")
+
+            # Return the collective results
+            return all_results
+
+        # Otherwise, return just the single result
+        return response.json()
+
+    # If code not 200, no results to process
+    warning("[!] API failure. Details:")
+    warning("    URL: %s", url)
+    warning("    Response Code: %s Reason: %s", response.status_code, response.reason)
+    return False
--- osint_tools/gitlab_checks.py
+++ osint_tools/gitlab_checks.py
@@ -6,150 +6,10 @@ These are called from the parent program, with the two core functions being:
     - process_projects
 """
 
-import re
-import os
 import logging as l
-import requests
 
+from api import gitlab
 
-API = 'https://gitlab.com/api/v4'
-API_KEY = None
-
-def api_get(url):
-    """
-    Helper function to interact with GitLab API using python requests
-
-    The important things here are:
-        - Adding the PRIVATE-TOKEN header based on env variable
-        - interacting with the pagination process via LINK headers
-          (https://docs.gitlab.com/ee/api/README.html#pagination)
-    """
-    api_key = os.getenv('GITLAB_API')
-
-    if api_key:
-        headers = {'PRIVATE-TOKEN': api_key}
-    else:
-        # Leaving this for possible future use, but program will actually
-        # sys.exit prior to this without an API key set.
-        headers = None
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        # The "Link" header is returned when there is more than one page of
-        # results. GitLab asks that we use this link instead of crafting
-        # our own.
-        if 'Link' in response.headers.keys():
-            # initialize a new variable to begin compounding multi-page
-            # results
-            all_results = response.json()
-            # Now, loop through until there is no 'next' link provided
-            pagenum = 2
-            while 'Link' in response.headers.keys() and 'rel="next"' in response.headers['Link']:
-                # Using print instead of logging, we don't want the per-page
-                # status update in the log file
-                print("[*] Processing page {}\r".format(pagenum), end='')
-
-                regex = re.compile(r'<([^<>]*?)>; rel="next"')
-                next_url = re.findall(regex, response.headers['Link'])[0]
-
-                # Add the individual response to the collective
-                response = requests.get(next_url, headers=headers)
-                if response.status_code == 200:
-                    all_results += response.json()
-                else:
-                    l.warning("[!] Error processing pagination URL: %s", next_url)
-                pagenum += 1
-
-            # We need a line break if we've counted pages
-            if pagenum > 1:
-                print("")
-
-            # Return the collective retuls
-            return all_results
-
-        # Otherwise, return just the single result
-        return response.json()
-
-    # If code not 200, no results to process
-    l.warning("[!] API failure. Details:")
-    l.warning("    URL: %s", url)
-    l.warning("    Response Code: %s Reason: %s", response.status_code, response.reason)
-    return False
-
-def get_current_user():
-    """
-    Gets the current username, based on API key.
-    """
-    details = api_get('{}/user'.format(API))
-
-    if not details:
-        return False
-
-    username = details['username']
-    return username
-
-def get_group(group):
-    """
-    Validates access to a group via the API
-    """
-    l.info("[*] Fetching group details for %s", group)
-    group_details = api_get('{}/groups/{}'.format(API, group))
-
-    if not group_details:
-        return False
-
-    return group_details
-
-def get_group_members(group):
-    """
-    Returns a list of all members of a group
-    """
-    members = []
-
-    l.info("[*] Fetching group members for %s", group)
-    details = api_get('{}/groups/{}/members'.format(API, group))
-
-    # We should now have a list of dictionary items, need to parse through
-    # each one to extract the member info.
-    for item in details:
-        members.append(item['username'])
-
-    return members
-
-def get_group_projects(group):
-    """
-    Returns a list of all projects belonging to a group
-    """
-    project_urls = []
-
-    l.info("[*] Fetching group projects for %s", group)
-    details = api_get('{}/groups/{}/projects'.format(API, group))
-
-    if not details:
-        pass
-
-    for item in details:
-        project_urls.append(item['http_url_to_repo'])
-
-    return project_urls
-
-def get_personal_projects(member):
-    """
-    Returns a list of all personal projects for a member
-    """
-    project_urls = []
-
-    l.info("[*] Fetching personal projects for %s", member)
-    details = api_get('{}/users/{}/projects'.format(API, member))
-
-    if not details:
-        return []
-
-    for item in details:
-        project_urls.append(item['http_url_to_repo'])
-
-    return project_urls
 
 def process_repos(repo):
     """
@@ -158,12 +18,14 @@ def process_repos(repo):
     l.info("Repos:  This feature is not yet implemented.")
     return
 
+
 def process_projects(projects):
     """
     Process a GitLab project
     """
     l.info("Projects:  This feature is not yet implemented.")
     return
+
 
 def process_groups(groups):
     """
@@ -174,16 +36,16 @@ def process_groups(groups):
     personal_projects = set()
 
     for group in groups:
-        group_details = get_group(group)
+        group_details = gitlab.get_group(group)
         if not group_details:
             l.warning("[!] %s not found, skipping", group)
             continue
 
-        members = get_group_members(group)
+        members = gitlab.get_group_members(group)
         for member in members:
-            personal_projects.update(get_personal_projects(member))
+            personal_projects.update(gitlab.get_personal_projects(member))
 
-        group_projects = get_group_projects(group)
+        group_projects = gitlab.get_group_projects(group)
 
         # Print / log all the gorey details
         l.info("GROUP: %s (%s)", group_details['name'],
