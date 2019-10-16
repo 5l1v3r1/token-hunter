e8668a682b3f58f32e0dd8fde500e1366ab2752c
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2019-10-16 08:57:20 -0400

    regex patterns are loaded

--- osint_tools/gitlab_snippets_monitor.py
+++ osint_tools/gitlab_snippets_monitor.py
@@ -1,3 +1,14 @@
+import json
+import os
 
-def get_secrets(content):
-    return []
+
+class GitLabSnippetMonitor:
+
+    def __init__(self):
+        with open(os.path.join(os.path.dirname(__file__), "../regexes.json"), 'r') as f:
+            self.regexes = json.loads(f.read())
+
+    def get_secrets(self, content):
+        if not content:
+            return []
+        return [content]
--- test_gitlab_snippets.py
+++ test_gitlab_snippets.py
@@ -18,3 +18,9 @@ def test_finds_gitlab_pat():
     content = "a8pt01843901sdf0-a_1"
     assert target.get_secrets(content) == [content]
 
+
+def test_regexes_are_loaded():
+    target = gitlab_snippets_monitor.GitLabSnippetMonitor()
+    assert len(target.regexes) > 0
+    assert target.regexes["GitLab PAT"] is not None
+
