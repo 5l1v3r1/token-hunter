ff371516bf04b8896a79fadf571fb4a3c894e5e0
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2019-10-16 12:15:40 -0400

    find a gitlab PAT in simple json and complex text block

--- osint_tools/gitlab_snippets_monitor.py
+++ osint_tools/gitlab_snippets_monitor.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 
 class GitLabSnippetMonitor:
@@ -7,8 +8,16 @@ class GitLabSnippetMonitor:
     def __init__(self):
         with open(os.path.join(os.path.dirname(__file__), "../regexes.json"), 'r') as f:
             self.regexes = json.loads(f.read())
+        for key in self.regexes:
+            self.regexes[key] = re.compile(self.regexes[key])
 
     def get_secrets(self, content):
+        result = {}
         if not content:
-            return []
-        return [content]
+            return result
+        for key in self.regexes:
+            match = self.regexes[key].search(content)
+            if match:
+                print(match)
+                result[key] = match.group(1)
+        return result
--- test_gitlab_snippets.py
+++ test_gitlab_snippets.py
@@ -1,22 +1,23 @@
+import textwrap
 from osint_tools import gitlab_snippets_monitor
 
 
 def test_handles_empty_string():
     target = gitlab_snippets_monitor.GitLabSnippetMonitor()
     content = ""
-    assert target.get_secrets(content) == []
+    assert target.get_secrets(content) == {}
 
 
 def test_handles_nil():
     target = gitlab_snippets_monitor.GitLabSnippetMonitor()
     content = None
-    assert target.get_secrets(content) == []
+    assert target.get_secrets(content) == {}
 
 
-def test_finds_gitlab_pat():
+def test_finds_simple_json_gitlab_pat():
     target = gitlab_snippets_monitor.GitLabSnippetMonitor()
-    content = "a8pt01843901sdf0-a_1"
-    assert target.get_secrets(content) == [content]
+    content = "API_KEY:a8pt01843901sdf0-a_1"
+    assert target.get_secrets(content) == {"API_KEY": "a8pt01843901sdf0-a_1"}
 
 
 def test_regexes_are_loaded():
@@ -24,3 +25,41 @@ def test_regexes_are_loaded():
     assert len(target.regexes) > 0
     assert target.regexes["GitLab PAT"] is not None
 
+
+def test_finds_gitlab_pat_in_text_block():
+    target = gitlab_snippets_monitor.GitLabSnippetMonitor()
+    content = textwrap.dedent("""\
+            using System.Collections.Generic;
+            using System.Runtime.CompilerServices;
+            
+            namespace NameSpace1
+            {
+                public static class DoubleExecutionPreventerExtensions
+                {
+                    private static readonly List<string> locks = new List<string>();
+            
+                    public static void Free(this object obj, [CallerMemberName] string caller = null)
+                    {
+                        string key = GetKey(obj, caller);
+                        locks.Remove(key);
+                    }
+            
+                    public static bool Lock(this object obj, [CallerMemberName] string caller = null)
+                    {
+                        string key = GetKey(obj, caller);
+            
+                        if (locks.Contains(key))
+                            return true;
+            
+                        locks.Add(key);
+                        return false;
+                    }
+            
+                    private static string GetKey(object instance, string caller)
+                    {
+                        return "-1a890cm-kforemg980="
+                    }
+                }
+            }
+        """)
+    assert target.get_secrets(content) == {"GitLab PAT": "-1a890cm-kforemg980="}
