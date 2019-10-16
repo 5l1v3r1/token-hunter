9035b3671efc909a376e7fb0534311a15a7f7b81
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2019-10-16 08:40:31 -0400

    move to a class for monitoring secrets to maintain state on regex set

--- test_gitlab_snippets.py
+++ test_gitlab_snippets.py
@@ -1,7 +1,20 @@
-from osint_tools import gitlab_snippets
+from osint_tools import gitlab_snippets_monitor
 
 
 def test_handles_empty_string():
+    target = gitlab_snippets_monitor.GitLabSnippetMonitor()
     content = ""
-    result = gitlab_snippets.get_secrets(content)
-    assert result == []
+    assert target.get_secrets(content) == []
+
+
+def test_handles_nil():
+    target = gitlab_snippets_monitor.GitLabSnippetMonitor()
+    content = None
+    assert target.get_secrets(content) == []
+
+
+def test_finds_gitlab_pat():
+    target = gitlab_snippets_monitor.GitLabSnippetMonitor()
+    content = "a8pt01843901sdf0-a_1"
+    assert target.get_secrets(content) == [content]
+
