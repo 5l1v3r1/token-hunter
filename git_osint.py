8f3c357af1eda94f2dfa8e53b372f6a67dd33924
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2019-10-18 11:38:59 -0400

    add an option to timestamp the output in order to be more compatible with docksint integration

--- git_osint.py
+++ git_osint.py
@@ -13,8 +13,9 @@ def main():
     log.configure(args.logfile)
     validate.environment()
 
-    info("##### Git_OSINT started at UTC %s from IP %s##### ",
-         time.get_current(datetime.timezone.utc), identity.get_public_ip())
+    if args.timestamp:
+        info("##### Git_OSINT started at UTC %s from IP %s##### ",
+             time.get_current(datetime.timezone.utc), identity.get_public_ip())
 
     validate.gitlab_api_keys(args)
 
@@ -24,7 +25,8 @@ def main():
     except KeyboardInterrupt:
         info("[!] Keyboard Interrupt, abandon ship!")
 
-    info("##### Git_OSINT finished at UTC %s ##### ", time.get_current(datetime.timezone.utc))
+    if args.timestamp:
+        info("##### Git_OSINT finished at UTC %s ##### ", time.get_current(datetime.timezone.utc))
 
 
 if __name__ == '__main__':
