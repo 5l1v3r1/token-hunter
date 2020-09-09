bfe6e23dcda3391c3f30b3c5ccc41eab5dd8195a
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2020-09-09 15:41:53 -0400

    stub in limit for job log searches and improve logging

--- gitlab/job_logs.py
+++ gitlab/job_logs.py
@@ -1,6 +1,6 @@
 from utilities import types, validate
 from api import gitlab
-from logging import info
+from logging import info, warning
 
 gitlab = gitlab.GitLab(types.Arguments().url)
 
@@ -9,10 +9,16 @@ def get_all(project_id, project_url):
     job_logs = []
     jobs = gitlab.get_jobs(project_id)
     if validate.api_result(jobs):
-        info("[*] Found %s jobs for project %s", len(jobs), project_url)
+        limit = 25
+        warning("[*] Found %s jobs for project %s.  Limiting scan depth to %s", len(jobs), project_url, limit)
+        i = 0
         for job in jobs:
+            info("[*] Retrieving job %s for %s which completed at %d", job['id'], project_url, job['finished_at'])
             job_log = gitlab.get_job_logs(project_id, job['id'])
             job_logs.append(types.JobLog(job['id'], job['web_url'], job_log))
+            i += 1
+            if i == limit:
+                break
     return job_logs
 
 
