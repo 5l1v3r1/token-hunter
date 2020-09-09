5d6149b29ea15f59dd0519022d196cc3e1ab3aee
Greg Johnson (codeEmitter) <gjohnson@gitlab.com>
2020-09-09 16:05:54 -0400

    implement depth for job logs

--- gitlab/job_logs.py
+++ gitlab/job_logs.py
@@ -3,17 +3,18 @@ from api import gitlab
 from logging import info, warning
 
 gitlab = gitlab.GitLab(types.Arguments().url)
+args = types.Arguments()
 
 
 def get_all(project_id, project_url):
     job_logs = []
     jobs = gitlab.get_jobs(project_id)
     if validate.api_result(jobs):
-        limit = 25
-        warning("[*] Found %s jobs for project %s.  Limiting scan depth to %s", len(jobs), project_url, limit)
+        limit = args.depth
+        warning("[*] Found %s jobs for project %s.  Limiting scan depth (--depth) to %s", len(jobs), project_url, limit)
         i = 0
         for job in jobs:
-            info("[*] Retrieving job %s for %s which completed at %d", job['id'], project_url, job['finished_at'])
+            info("[*] Retrieving job %s for %s which completed at %s", job['id'], project_url, job['finished_at'])
             job_log = gitlab.get_job_logs(project_id, job['id'])
             job_logs.append(types.JobLog(job['id'], job['web_url'], job_log))
             i += 1
